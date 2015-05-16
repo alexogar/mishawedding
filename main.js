42e8c5c4a110ef878cbb95fbb883aa3c2c1e72df
Aleksei Ogarkov <alexey.ogarkov@gmail.com>
2015-05-16 15:10:23 +0300

    Quest added

--- main.js
+++ main.js
@@ -1,73 +1,31 @@
 (function(){jQuery(document).ready(function() {
-	var noSlide = {
-		img: "img/14.jpg"
-	};
-	var questSlides = [
-		{
-			img : "img/0.jpg",
-			text : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate aspernatur nulla assumenda minus similique reprehenderit modi, quaerat commodi repellendus mollitia accusantium nam blanditiis hic laudantium quasi reiciendis debitis nemo consequatur?",
-			yes_text : "asdasd",
-			no_text : "asdasdasd"
-		},
+	var change = function(from, to) {
+		$(from).fadeOut("slow", function() {
+			$(to).fadeIn("slow", function(){
+				$("html, body").animate({ scrollTop: $(document).height() }, "slow");
+			});
+			
+		});
 
-		{
-			img : "img/01.jpg",
-			text : "qweqweqweqweqwe"
-		},
-
-		{
-			img : "img/02.jpg",
-			text : "dsfkaf;ad;fnasd;fnas;dfas;dofnas;df",
-			yes_text : "asdasd",
-			no_text : "asdasdasd"
-		},
-
-		{
-			img : "img/03.jpg",
-			text : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi ipsum, autem eligendi quam sapiente alias nihil sunt suscipit ipsam tenetur dolorem nobis! Quo quam architecto in corporis quibusdam eius placeat."
-		},
-
-		{
-			img : "img/04.jpg",
-			text : "aasdasdasdasdas"
-		}
-	];
-
-	var currentSlide = 0;
-
-	var showSlide = function(num) {
-		currentSlide = num;
-
-		var slide = questSlides[currentSlide];
-
-		jQuery("#quest-slide-img").attr('src', slide.img);
-		jQuery("#quest-slide-text > p").html(slide.text);
-
-		var yt = slide.yes_text || "Yes";
-		var nt = slide.no_text || "No";
-
-		jQuery("#quest-yes-button").text(yt);
-		jQuery("#quest-no-button").text(nt);
 	};
 
-	jQuery("#quest-yes-button").click(function() {
-		showSlide(currentSlide+1);
+	jQuery(".quest_int_link").click(function(e) {
+		e.preventDefault();
+		var ref = jQuery(this).attr('ref');
+		change(jQuery(this).closest('.slide'), ref);
 	});
 
-	//showSlide(0)
-
-	jQuery("#faq_link").click(function () {
-
-
-		jQuery("#main_screen > div").hide();
-		jQuery("#faq_content").show();
+	jQuery(".quest_show_link").click(function() {
+		var ref = jQuery(this).attr('ref');
+		jQuery(ref).fadeIn('slow');
+		$("html, body").animate({ scrollTop: $(document).height() }, "slow");
 	});
 
-	jQuery("#when_link").click(function () {
+	jQuery("#quest_link").click(function() {
+		jQuery(".links").fadeOut(function(){
+			change("#beginning","#quest_start");
+		});
 
-
-		jQuery("#main_screen > div").hide();
-		jQuery("#when_content").show();
 	});
 
 })})();
